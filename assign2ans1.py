5e133791769712f6515fe2912985bde45b31708d
MOHD ANAS <mohdanas7223@gmail.com>
2025-02-27 19:06:30 +0530

    Add files via upload

---
+++ assign2ans1.py
@@ -0,0 +1,9 @@
+## FFIBONACCI SEQUENCE
+def fibonacci(n):
+    a,b = 0,1
+    for _ in range(n):
+        print(a, end=" ")
+        a,b=b,a+b
+
+n = int(input("enter the fibonacci number: "))
+fibonacci(n)
